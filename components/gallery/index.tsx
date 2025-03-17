da2c93aed20802319b04f8080b996570b0e09f7b
m2nnari <meelis.mannari@tptlive.ee>
2025-03-17 02:12:53 +0200

    fix

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -5,7 +5,7 @@ import { Flex } from "../styles/flex";
 const images = [
   "/image1.jpg",
   "/image2.jpg",
-  "/image3.jpg",
+  "/image333.jpg",
   "/image4.jpg",
   "/image5.jpg",
   "/image6.jpg",
